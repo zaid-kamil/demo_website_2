9adeb95ae5253cb6b80227c64309c618e817b948
Zaid Kamil <xaidmetamorphos@gmail.com>
2023-03-06 18:27:13 +0530

    data passing

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('blog/categories/', views.categories, name="categories"),
+    path('blog/articles/', views.articles, name="articles"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,23 @@ def home(request):
     address = 'The Citadel'
     ctx = {'athr':author, 'age':age, 'addr':address}
     return render(request, 'home.html',context=ctx)
+
+def categories(request):
+    cats = ['Python', 'Django', 'Web Development', 'Programming']
+    return render(request, 'categories.html',
+                  context={'categories':cats})
+
+def articles(request):
+    data = [
+        {'title':'First Article', 
+         'author':'Waxllium Ladrian', 
+         'date':'2020-01-01'},
+        {'title':'The Basin of Life',
+         'author':'Steris',
+         'date':'2022-01-02'},
+        {'title':'The Second Article',
+         'author':'Waxllium Ladrian',
+         'date':'2020-01-03'},
+    ]
+    return render(request, 'articles.html',
+                    context={'articles':data})
