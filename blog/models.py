83d3eb7731e787ff2c63c3fe1dd491eb160657c4
Zaid Kamil <xaidmetamorphos@gmail.com>
2023-03-27 18:19:43 +0530

    models #1

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    desc = models.TextField()
+    created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+class Contact(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=122)
+    mobile = models.CharField(max_length=12)
+    message = models.TextField()
+    date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
